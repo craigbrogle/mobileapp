0ac49de450824c5479f65ac4127bd51ad515cc11
Juan Ignacio Laube <juanlaube@icloud.com>
2018-06-04 09:40:13 -0300

    :fire: Remove unnecesary code

--- Toggl.Foundation/Serialization/IJsonSerializer.cs
+++ Toggl.Foundation/Serialization/IJsonSerializer.cs
@@ -4,12 +4,6 @@ namespace Toggl.Foundation.Serialization
 {
     public interface IJsonSerializer
     {
-        string Serialize(object toSerialize);
-
-        bool TrySerialize(object toSerialize, out string result);
-
         T Deserialize<T>(string json);
-
-        bool TryDeserialize<T>(string json, out T result);
     }
 }
--- Toggl.Foundation/Serialization/JsonSerializer.cs
+++ Toggl.Foundation/Serialization/JsonSerializer.cs
@@ -4,32 +4,7 @@ namespace Toggl.Foundation.Serialization
 {
     public sealed class JsonSerializer : IJsonSerializer
     {
-        public string Serialize(object toSerialize)
-            => JsonConvert.SerializeObject(toSerialize);
-
-        public bool TrySerialize(object toSerialize, out string result)
-        {
-            try {
-                result = Serialize(toSerialize);
-                return true;
-            } catch (JsonSerializationException) {
-                result = default(string);
-                return false;
-            }
-        }
-
         public T Deserialize<T>(string json)
             => JsonConvert.DeserializeObject<T>(json);
-
-        public bool TryDeserialize<T>(string json, out T result)
-        {
-            try {
-                result = Deserialize<T>(json);
-                return true;
-            } catch (JsonSerializationException) {
-                result = default(T);
-                return false;
-            }
-        }
     }
 }
