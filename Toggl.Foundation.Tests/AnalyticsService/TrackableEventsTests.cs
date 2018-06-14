10dd2520f7bbbbd005b11646fe6f71968cc806b0
kornelijepetak <kornelije@gmail.com>
2018-06-14 11:10:59 +0200

    :white_check_mark: Do a test cleanup

--- Toggl.Foundation.Tests/AnalyticsService/TrackableEventsTests.cs
+++ Toggl.Foundation.Tests/AnalyticsService/TrackableEventsTests.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Diagnostics;
-using FluentAssertions;
 using NSubstitute;
 using Toggl.Foundation.Analytics;
 using Xunit;
@@ -14,7 +12,7 @@ namespace Toggl.Foundation.Tests.AnalyticsService
         private const string OverriddenEventName = "Overridden";
         private const string OverriddenParameterName = "CreationOrigin";
 
-        private class RandomEvent : ITrackableEvent
+        private sealed class RandomEvent : ITrackableEvent
         {
             public int Ergalactic { get; } = 199;
             public string Theory { get; } = "Quantum entanglement";
@@ -23,7 +21,7 @@ namespace Toggl.Foundation.Tests.AnalyticsService
         }
 
         [DisplayName(OverriddenEventName)]
-        private class StartTimeEntryEvent : ITrackableEvent
+        private sealed class StartTimeEntryEvent : ITrackableEvent
         {
             [DisplayName(OverriddenParameterName)]
             public TimeEntryStartOrigin Origin { get; } = TimeEntryStartOrigin.Timer;
@@ -36,14 +34,14 @@ namespace Toggl.Foundation.Tests.AnalyticsService
             public bool IsBillable { get; } = false;
         }
 
-        public IAnalyticsService AnalyticsService { get; } = Substitute.For<IAnalyticsService>();
+        private readonly IAnalyticsService analyticsService = Substitute.For<IAnalyticsService>();
 
         private RandomEvent createRandomEvent() => new RandomEvent();
         private StartTimeEntryEvent createStartTimeEntryEvent() => new StartTimeEntryEvent();
 
         public TrackableEventsTests()
         {
-            AnalyticsService = Substitute.For<IAnalyticsService>();
+            analyticsService = Substitute.For<IAnalyticsService>();
         }
 
         [Fact]
@@ -51,9 +49,9 @@ namespace Toggl.Foundation.Tests.AnalyticsService
         {
             var randomEvent = createRandomEvent();
 
-            randomEvent.TrackWith(AnalyticsService);
+            randomEvent.TrackWith(analyticsService);
 
-            AnalyticsService.Received().Track(nameof(RandomEvent),
+            analyticsService.Received().Track(nameof(RandomEvent),
                 Arg.Is<Dictionary<string, string>>(dict =>
                                                    dict[nameof(randomEvent.Ergalactic)] == randomEvent.Ergalactic.ToString() &&
                                                    dict[nameof(randomEvent.Theory)] == randomEvent.Theory.ToString() &&
@@ -67,9 +65,9 @@ namespace Toggl.Foundation.Tests.AnalyticsService
         {
             var randomEvent = createStartTimeEntryEvent();
 
-            randomEvent.TrackWith(AnalyticsService);
+            randomEvent.TrackWith(analyticsService);
 
-            AnalyticsService.Received().Track(OverriddenEventName, Arg.Any<Dictionary<string, string>>());
+            analyticsService.Received().Track(OverriddenEventName, Arg.Any<Dictionary<string, string>>());
         }
 
         [Fact]
@@ -77,9 +75,9 @@ namespace Toggl.Foundation.Tests.AnalyticsService
         {
             var randomEvent = createStartTimeEntryEvent();
 
-            randomEvent.TrackWith(AnalyticsService);
+            randomEvent.TrackWith(analyticsService);
 
-            AnalyticsService.Received().Track(Arg.Any<string>(),
+            analyticsService.Received().Track(Arg.Any<string>(),
                 Arg.Is<Dictionary<string, string>>(d => d[OverriddenParameterName] == TimeEntryStartOrigin.Timer.ToString()
             ));
         }
